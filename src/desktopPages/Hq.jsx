cdb669b2fa12db5666e51d3aa5e28172bda06981
RockGyuLee <rmfoszld@naver.com>
2021-05-03 17:57:34 +0900

    Hq Project Component 생성

--- src/desktopPages/Hq.jsx
+++ src/desktopPages/Hq.jsx
@@ -27,43 +27,28 @@ const skills = [
     {
         name : "FrontEnd",
         img : "./imgs/front.png",
-        contents : "HTML CSS JavaScript JQuery \n React ReactNative SPA 개발 \n AndroidStudio 개발"
+        contents : "HTML · CSS · JavaScript · JQuery \n React · ReactNative · SPA 개발 \n AndroidStudio 개발"
     },
     {
         name : "BackEnd",
         img : "./imgs/back.png",
-        contents : "SpringBoot NodeJs API  Django구현 \n MySQL DB 스키마 설계 \n"
+        contents : "SpringBoot · NodeJs · Django API구현 \n MySQL DB 스키마 설계 \n"
     },
     {
         name : "Collaboration",
         img : "./imgs/collaboration.png",
-        contents : "Git 버전관리 Webpack 모듈 번들러 사용 \n Firebase 플랫폼 사용\n Figma UI/UX 협업"
+        contents : "Git 버전관리 · Webpack 모듈 번들러 사용 \n Firebase 플랫폼 사용\n Figma UI/UX 협업"
     }
 ]
 
 function HqIntro(props){
 
     return(
-        <Container width="1200px" height="500px" css={{
+        <Container width="auto" height="500px" css={{
             position : 'relative',
             textAlign : 'center',
             margin : '0 auto'
         }}>
-            {/* <div style={{marginTop : "20px"}}>
-                <Span size="xxxl"> - Myself -</Span>
-                <Ptag size="lg">
-                    Rlay라는 단어는 “락규가 즐거워한다.”를 <br></br>
-                    영어이름 + 영단어의 조합 Rock + Play 를 합친 단어입니다.<br></br>
-                    <br></br>
-                    <Typed 
-                        strings = {strArray}
-                        startDelay = {500}
-                        typeSpeed = {100}
-                        backSpeed = {50}
-                        loop
-                    />
-                </Ptag>
-            </div> */}
             <div style={{marginTop : "20px"}}>
                 <Span size="xxxl"> - Skills -</Span>
                 <Ptag size="lg">
@@ -94,15 +79,28 @@ function HqIntro(props){
     )
 }
 
+function HqProject(props){
+    return (
+        <Container width="auto" height="500px" css={{
+            position : 'relative',
+            textAlign : 'center',
+            margin : '0 auto',
+            backgroundColor : '#F2F2F2'
+        }}>
+            <div style={{marginTop : "20px"}}>
+                <Span size="xxxl"> - Project -</Span>
+            </div>
+        </Container>
+    )
+}
+
 function Hq(props){
     return(
         <div style={{overflow : 'scroll'}}>
             <div style={{
                 position : 'relative', width : "100%", height:"500px", backgroundColor : "black",
-                // backgroundImage : "url(/imgs/r.png)",
-                // backgroundSize : "contain"
             }}>
-                <div style={{float : 'right', margin : '50px 100px', width : '700px'}}>
+                <div style={{float : 'right', margin : '5%', width : '600px'}}>
                     <Ptag size="xl" color="white">
                         Rlay라는 단어는 “락규가 즐거워한다.”를 <br/>
                         영어이름 + 영단어의 조합 Rock + Play 를 합친 단어입니다.<br/>
@@ -116,10 +114,10 @@ function Hq(props){
                         />
                     </Ptag>
                 </div>
-                <img src='./imgs/r.png'/>
-                
+                <img style={{position : 'absolute'}} src='./imgs/r.png'/>
             </div>
             <HqIntro/>
+            <HqProject />
         </div>
     )
 }
